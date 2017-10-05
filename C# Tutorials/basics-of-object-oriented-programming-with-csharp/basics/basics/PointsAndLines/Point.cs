1f91e6e8436018838488c715825ebfb5e911bdd7
arunnie92 <arunnie92@users.noreply.github.com>
2017-10-04 18:20:48 -0700

    C# folder created and add udemy class
    Udemy Class | Basics of Object Oriented Programming with C#

---
+++ C# Tutorials/basics-of-object-oriented-programming-with-csharp/basics/basics/PointsAndLines/Point.cs
@@ -0,0 +1,62 @@
+﻿namespace PointAndLines
+{
+    class User
+    {
+
+        public static int currentID;
+
+        public const int HEIGHT = 180; // const must set when created, and same for all instances of the class 
+
+        public readonly int ID; // readonly can be set later in the code
+
+        private string username;
+        private int password;
+
+        // getter & setter
+        public string Username
+        {
+            get
+            {
+                return "The username is " + username;
+            }
+        }
+
+        public int Password
+        {
+            set
+            {
+                if (value >= 4 && value <= 10)
+                {
+                    password = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Oops, this is not a valid password! Please use a password between 4 and 10 characters");
+                }
+            }
+        }
+
+        public User()
+        {
+            currentID++;
+            ID = currentID; 
+        }
+
+        public User(string username)
+        {
+            currentID++;
+            ID = currentID;
+            this.username = username;
+        }
+
+        
+    }
+}
+
+
+// namespace allows you to organize classes and code better
+
+// 'this' - gives you access to the fields of your class 
+//        - (optional) lets you distinguish between two variables that have the same name
+
+// using a different combinations of get and set, we can control if a given Property is Read-Only, Write-only or Read-Write   
---
+++ C# Tutorials/basics-of-object-oriented-programming-with-csharp/basics/basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using PointAndLines;
+
+class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        User user = new User("Arunn");
+
+        User secondUser = new User();
+
+        User thirdUser = new User();
+        
+        System.Console.WriteLine(user.ID);
+        System.Console.WriteLine(secondUser.ID);
+        System.Console.WriteLine(thirdUser.ID);
+        System.Console.WriteLine(User.currentID);
+    }
+}
+
+// Static Field
+// (Static members in general) are not dependant on the instance of a given class
+// they can be accessed directly from the class without instatiating it. 
+// the value of a static field is not dependant on a Class Instance.
+// the static field is "shared" amongst all instances of the Class and they all can modify or access it iternally
+// useful to share a varibale between all instances of a class
+
+// const - defined value hardcode in the code
+// readonly - value assigned at runtime through code logic
+// constants of any kind cannot change their valu once it's set in a constructor or initializer 
+// usually have const variables all capitlized or camel case or with _
+// const constants are static by default
